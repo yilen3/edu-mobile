9812cc147dfbf984aaa8c106b8fd27ebf33c26cb
yilen3 <1413599820@qq.com>
2022-04-23 18:22:39 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
 <!-- 根路由出口 -->
-  <router-view/>
+  <!-- <router-view/> -->
+  <div>
+    <van-button>按钮</van-button>
+    <div class="box">示例元素</div>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.box {
+  width: 100px;
+  height: 100px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入 lib-flexible
+import 'amfe-flexible'
+
+// 引入 vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 将 vant 注册为 vue 插件
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
