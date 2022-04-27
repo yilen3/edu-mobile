2628f50d5459ab3b09623646092c142293dea022
yilen3 <1413599820@qq.com>
2022-04-27 20:00:23 +0800

    text

--- src/views/pay/index.vue
+++ src/views/pay/index.vue
@@ -1,12 +1,178 @@
 <template>
-  <div class="pay">支付功能</div>
+  <div class="pay">
+    <van-cell-group>
+      <van-cell class="course-info">
+        <!-- 图片 -->
+        <img :src="course.courseImgUrl">
+        <!-- 课程信息 -->
+        <div class="price-info">
+          <div class="course-name" v-text="course.courseName"></div>
+          <div class="discounts">￥{{ course.discounts }}</div>
+        </div>
+      </van-cell>
+      <!-- 账户信息 -->
+      <van-cell class="account-info">
+        <div>购买信息</div>
+        <div>购买课程后使用此账号登录【拉钩教育】学习课程</div>
+        <div class="user-name">当前账号：{{ username }}</div>
+      </van-cell>
+      <!-- 支付功能 -->
+      <van-cell class="pay-channel">
+        <!-- 支付 -->
+        <div class="title">
+          <p>支付方式</p>
+          <van-radio-group v-model="radio">
+            <van-cell-group>
+              <van-cell @click="radio = '1'">
+                <template #title>
+                  <img src="http://www.lgstatic.com/lg-app-fed/pay/images/wechat_b787e2f4.png">
+                  <span>微信支付</span>
+                </template>
+                <template #right-icon>
+                  <van-radio name="1" />
+                </template>
+              </van-cell>
+              <van-cell @click="radio = '2'">
+                <template #title>
+                  <img src="http://www.lgstatic.com/lg-app-fed/pay/images/ali_ed78fdae.png">
+                  <span>支付宝支付</span>
+                </template>
+                <template #right-icon>
+                  <van-radio name="2" />
+                </template>
+              </van-cell>
+            </van-cell-group>
+          </van-radio-group>
+        </div>
+        <van-button>￥{{ course.discounts }} 立即支付</van-button>
+      </van-cell>
+    </van-cell-group>
+  </div>
 </template>
 
 <script>
+import { getCourseById } from '@/services/course'
 export default {
-  name: 'Pay'
+  name: 'Pay',
+  props: {
+    courseId: {
+      type: [String, Number],
+      required: true
+    }
+  },
+  data () {
+    return {
+      // 课程信息
+      course: [],
+      radio: '1'
+    }
+  },
+  created () {
+    this.loadCourse()
+  },
+  methods: {
+    async loadCourse () {
+      const { data } = await getCourseById({
+        courseId: this.courseId
+      })
+      this.course = data.content
+      console.log(data)
+    }
+  },
+  computed: {
+    username () {
+      return this.$store.state.user.organization.replace(/(\d{3})\d{4}(\d{4})/, '$1****$2')
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.pay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+.van-cell-group {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+}
+.course-info {
+  height: 170px;
+  padding: 40px 20px 0;
+  margin-bottom: 10px;
+}
+.course-info img {
+  width: 80px;
+  height: 107px;
+  border-radius: 10px;
+}
+.course-info .van-cell__value {
+  display: flex;
+}
+.course-info .price-info {
+  display: flex;
+  height: 107px;
+  padding: 5px 20px;
+  box-sizing: border-box;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.price-info .course-name {
+  font-size: 16px;
+}
+.price-info .discounts {
+  font-size: 22px;
+  font-weight: 700;
+  color: #ff7452;
+}
+.account-info {
+  height: 120px;
+  margin-bottom: 10px;
+}
+.account-info div:nth-child(2) {
+  font-size: 12px;
+  color: #999;
+}
+.account-info .user-name {
+  margin: 20px 0 10px;
+  font-size: 16px;
+}
+.pay-channel {
+  flex: 1;
+}
+.pay-channel .van-cell {
+  padding: 20px 10px;
+}
+.pay-channel .van-cell__value {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.pay-channel .van-cell__title {
+  display: flex;
+  align-items: center;
+}
+.pay-channel .van-cell img {
+  width: 28px;
+  height: 28px;
+}
+.pay-channel .van-cell span {
+  font-size: 16px;
+  margin-left: 10px;
+}
+// 右侧 radio 选中颜色，深度选择
+::v-deep .van-radio__icon--checked .van-icon {
+  background-color: #fbc546;
+  border-color: #fbc546;
+}
+// 底部按钮样式
+.pay-channel .van-button {
+  background: linear-gradient(270deg, #faa83e, #fbc546);
+  border-radius: 20px;
+  margin-bottom: 10px;
+  font-size: 18px;
+}
 </style>
